a473640d6444d9bcf87ed40d09f64f2e9cae7114
nikhil730 <nikhilbhalla196@gmail.com>
2023-11-11 16:59:09 +0530

    ..

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -87,7 +87,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Nikhil Bhalla</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
