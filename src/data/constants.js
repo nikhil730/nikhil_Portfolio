c8c257867957e5659b91c8752d1fa90e3356e3f4
nikhil730 <nikhilbhalla196@gmail.com>
2024-02-23 01:45:52 +0530

    .

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/nikhil730",
   resume:
-    "https://drive.google.com/file/d/1Bwai7Cl5kxuOvh6Asn_K7UZLuFdX1L_c/view?usp=drive_link",
+    "https://drive.google.com/file/d/1sCluxuPOjMDUmKBcnefrRebEPb9JoArL/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/nikhil-bhalla-116704206/",
   insta: "https://www.instagram.com/nikhil.bhalla/",
 };
